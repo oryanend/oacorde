eb54e5f3df050d6a088ecb5e6bcd71b69c01c69c
oryanend <ryanyamim@gmail.com>
2025-12-27 18:40:38 -0300

    feat: create `tela-breve` component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TelaBreveComponent} from './components/tela-breve/tela-breve.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TelaBreveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
