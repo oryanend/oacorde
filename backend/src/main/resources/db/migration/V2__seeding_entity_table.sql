ee7b9414f62a85ba2e1d547570fbafe0fcc2b836
oryanend <ryanyamim@gmail.com>
2026-01-01 16:44:31 -0300

    feat: add user table and seeding migrations

---
+++ backend/src/main/resources/db/migration/V2__seeding_entity_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user (id, username, email, password)
+VALUES
+    (RANDOM_UUID(), 'John Marston', 'jm@test.com', 'jackmarston123'),
+    (RANDOM_UUID(), 'Arthur Morgan', 'am@test.com', 'johnmorgan123'),
+    (RANDOM_UUID(), 'Dutch Van Der Linde ', 'dutch@test.com', 'igotaplanarthur123');
