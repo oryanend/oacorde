22c0fc11353efefd30a73513718b4efabe56b803
oryanend <ryanyamim@gmail.com>
2026-01-04 02:30:26 -0300

    chore: add `UserDTOFactory` to tests

--- backend/src/test/java/com/oryanend/backend/factories/UserDTOFactory.java
+++ backend/src/test/java/com/oryanend/backend/factories/UserDTOFactory.java
@@ -1,5 +1,19 @@
 package com.oryanend.backend.factories;
 
-public class UserFactory {
-    
+import com.oryanend.backend.dto.UserDTO;
+import com.oryanend.backend.entities.User;
+import net.datafaker.Faker;
+
+public class UserDTOFactory {
+  private static final Faker faker = new Faker();
+
+  public static UserDTO createUserDTO(String username, String email, String password) {
+    User user =
+        new User(
+            null,
+            username != null ? username : faker.name().firstName(),
+            email != null ? email : faker.internet().emailAddress(),
+            password != null ? password : faker.text().text());
+    return new UserDTO(user);
+  }
 }
